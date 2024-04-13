60182ce6f6b33fd15fc798fe34d846a9992d70a3
slvDev <slava.serbov@gmail.com>
2024-04-13 21:35:16 +0300

    implement print_issue_in_console to InterfaceAndContractDetector

--- crates/forge/bin/cmd/lint/detectors/mod.rs
+++ crates/forge/bin/cmd/lint/detectors/mod.rs
@@ -1,10 +1,11 @@
-use foundry_compilers::artifacts::Ast;
+use foundry_compilers::artifacts::{Ast, Node};
 
 // Base trait for detectors
 pub trait Detector {
     fn title(&self) -> String;
     fn description(&self) -> String;
     fn detect(&self, ast: &Ast);
+    fn print_issue_in_console(&self, interface: &Node, contract: &Node);
 }
 
 pub mod nc;
--- crates/forge/bin/cmd/lint/detectors/nc/interface_and_contract.rs
+++ crates/forge/bin/cmd/lint/detectors/nc/interface_and_contract.rs
@@ -1,5 +1,7 @@
+use crate::cmd::lint::utils::get_full_line;
+
 use super::super::Detector;
-use foundry_compilers::artifacts::{Ast, NodeType};
+use foundry_compilers::artifacts::{Ast, Node, NodeType};
 
 
 pub struct InterfaceAndContractDetector {
@@ -25,30 +27,28 @@ impl Detector for InterfaceAndContractDetector {
     }
 
     fn detect(&self, ast: &Ast) {
-        let mut has_interface = None;
-        let mut has_contract = None;
+        let mut interface = None;
+        let mut contract = None;
 
         for node in ast.nodes.iter() {
             if NodeType::ContractDefinition == node.node_type {
                 if let Some(kind) = node.other.get("contractKind").and_then(|v| v.as_str()) {
                     match kind {
-                        "interface" => has_interface = Some(&node.src),
-                        "contract" => has_contract = Some(&node.src),
+                        "interface" => interface = Some(node),
+                        "contract" => contract = Some(node),
                         _ => {}
                     }
                 }
             }
         }
-        if has_interface.is_some() && has_contract.is_some() {
-            println!("{}\n{}", self.title(), self.description());
-            // TODO: add issues to return if report will be dumped to a file
-
-            // Should be better way to print the issue in code
-            // In current AST I can get only start and length of the node
-            // or start and length of the name
-
-            // if there no way to extract CodeLocation from cached AST
-            // Should use solang-parser to create AST 
+        if interface.is_some() && contract.is_some() {
+            self.print_issue_in_console(interface.unwrap(), contract.unwrap());
         }
+    }
+
+    fn print_issue_in_console(&self, interface: &Node, contract: &Node) {
+        println!("{}\n", self.title());
+        println!("{}", get_full_line(&self.code, interface.src.start));
+        println!("{}", get_full_line(&self.code, contract.src.start));
     }
 }
